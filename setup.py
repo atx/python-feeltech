618ec055970daf83453ffd1839f8bc01d0525934
Josef Gajdusek <atx@atx.name>
2015-08-29 19:30:39 +0200

    fytool: Add

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name = "python-feeltech",
     version = "0.1",
     packages = ["feeltech"],
+    scripts = ["bin/fytool"],
     description = "Python library for controlling FeelTech FY32xx waveform generators ",
     author = "Josef Gajdusek",
     author_email = "atx@atx.name",
